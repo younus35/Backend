0fe7027cf6d130ceb4a5ce99bf664a1c9efa00ce
younus35 <younuscode35@gmail.com>
2024-02-25 04:46:32 +0530

    Json + nodemon

--- javascript/reqres2.js
+++ javascript/reqres2.js
@@ -4,5 +4,6 @@ const routes = require('./routes');
 //routes cam be obj when we write module.exports as an object the we could acces the
 //specific function by routes.requestHandler(routes.key) in the below argument
 const server = http.createServer(routes);
+console.log("nodemon");
 
 server.listen(3000);
